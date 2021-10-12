0149bc3075da1823528498dd0c2106479c3a843b
WEIKIAT <kangweikiat@gmail.com>
2021-10-12 16:01:09 +0800

    Import fxml

--- classification/src/main/java/solution/upload/ASL_Classification_Load.java
+++ classification/src/main/java/solution/upload/ASL_Classification_Load.java
@@ -1,9 +1,9 @@
-package solution;
+package solution.upload;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.Group;
-import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class ASL_Classification_Load extends Application {
@@ -21,14 +21,10 @@ public class ASL_Classification_Load extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World");
         Group root = new Group();
-        Scene scene = new Scene(root, 300, 250);
-        Button btn = new Button();
-        btn.setLayoutX(100);
-        btn.setLayoutY(80);
-        btn.setText("Hello World");
-        btn.setOnAction( actionEvent ->
-                System.out.println("Hello World"));
-        root.getChildren().add(btn);
+        Scene scene = new Scene(root,800, 500);
+        scene.setFill(Color.ANTIQUEWHITE);
+
+        primaryStage.setTitle("ASL Application");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
