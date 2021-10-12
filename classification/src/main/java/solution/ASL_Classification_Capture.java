359b428038e297884859660123bca440da186bc8
WEIKIAT <kangweikiat@gmail.com>
2021-10-12 15:24:55 +0800

    Object Detection Initial Commit

---
+++ classification/src/main/java/solution/ASL_Classification_Capture.java
@@ -0,0 +1,16 @@
+package solution;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ASL_Classification_Capture extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
